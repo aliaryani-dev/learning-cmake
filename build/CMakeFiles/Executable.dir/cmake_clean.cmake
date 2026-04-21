f4bd39c903da900d01bffa886e48957a186b3b52
Ali Aryani <lenesis@proton.me>
2026-04-21 18:32:10 +0330

    hello world

---
+++ build/CMakeFiles/Executable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Executable.dir/link.d"
+  "CMakeFiles/Executable.dir/main.cpp.o"
+  "CMakeFiles/Executable.dir/main.cpp.o.d"
+  "Executable"
+  "Executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
