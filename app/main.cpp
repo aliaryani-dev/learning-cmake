7779ce830aebb1c07ca54bc33f2fe4aaadbc12b6
Ali Aryani <lenesis@proton.me>
2026-04-23 20:38:37 +0330

    external modules

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,13 @@
 #include "my_lib.h"
 #include "config.hpp"
+#include "nlohmann/json.hpp"
 #include <print>
 using namespace std;
 int main() {
     print_hello();
     println("Project name: {}",project_name);
     println("Version : {}", project_version);
+    println("JSON Lib Version: {}.{}.{}",NLOHMANN_JSON_VERSION_MAJOR,NLOHMANN_JSON_VERSION_MINOR,NLOHMANN_JSON_VERSION_PATCH);
 
     return 0;
 }
