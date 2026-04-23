9cd712aedb206f82d47d1d8c9af58e07b00682ec
Ali Aryani <lenesis@proton.me>
2026-04-23 17:09:44 +0330

    config file

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,11 @@
 #include "my_lib.h"
-
+#include "config.hpp"
+#include <print>
+using namespace std;
 int main() {
     print_hello();
+    println("Project name: {}",project_name);
+    println("Version : {}", project_version);
+
     return 0;
 }
